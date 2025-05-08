a1ea0aa54025b4a383b6941eb45d00178d1c1482
Yan <podoyo23@gmail.com>
2025-05-08 21:03:13 +0800

    add: keydown move

--- src/components/HomeMap.vue
+++ src/components/HomeMap.vue
@@ -2,8 +2,34 @@
 import mapImage from "@/assets/HomeMap.png"
 import CatImage from "@/assets/logo.png"
 
-import { ref, onMounted } from "vue"
+import { ref, onMounted, onUnmounted } from "vue"
+// 監聽鍵盤
+const handleKeydown = (e) => {
+	// console.log("你按下的鍵是：", e.key)
+	switch (e.key) {
+		case "w":
+		case "W":
+			posY.value -= step
+			break
+		case "s":
+		case "S":
+			posY.value += step
+			break
+		case "a":
+		case "A":
+			posX.value -= step
+			break
+		case "d":
+		case "D":
+			posX.value += step
+			break
+	}
+}
 
+// 離開元件時記得清除事件監聽
+onUnmounted(() => {
+	window.removeEventListener("keydown", handleKeydown)
+})
 // 定義貓咪移動的位置
 const posX = ref(0)
 const posY = ref(0)
@@ -13,12 +39,14 @@ const step = 10 // 貓咪移動的步伐
 onMounted(() => {
 	const screenWidth = window.innerWidth
 	const screenHeight = window.innerHeight
-	const catSize = 48 //對應 w-12 h-12
+	const catSize = 64 //對應 w-12 h-12
+	// 當鍵盤被按下時，自動執行 handleKeydown，並傳入 e
+	window.addEventListener("keydown", handleKeydown)
 
 	// 水平
 	posX.value = screenWidth / 2 - catSize / 2 //正中央
 	// 垂直底部往上 40px
-	posY.value = screenHeight / 2 - catSize + 45 // 離底部一點點上方
+	posY.value = screenHeight - catSize - 40 // 離底部一點點上方
 })
 </script>
 <template>
@@ -26,13 +54,13 @@ onMounted(() => {
 	<div class="w-screen h-screen relative overflow-hidden">
 		<img class="map-bg" :src="mapImage" alt="web-homepage" />
 		<!-- 移動貓咪 -->
-		<img class="absolute w-12 h-12" :src="CatImage" :style="{ top: `${posY}px`, left: `${posX}px` }" alt="CatImage" />
+		<img class="absolute w-16 h-16" :src="CatImage" :style="{ top: `${posY}px`, left: `${posX}px` }" alt="CatImage" />
 	</div>
 </template>
 <style>
 .map-bg {
 	width: 100%;
 	height: 100%;
-	object-fit: contain;
+	object-fit: cover;
 }
 </style>
